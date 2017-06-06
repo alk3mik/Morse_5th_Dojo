b41e61c65495f030aef24b9442d4ba88fae18e30
alk3mik <mozz76@gmail.com>
2017-06-06 02:42:54 +0200

    It does not work yet, but we are on the good trajectory...

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let Morse = require('./morse');
+
+Morse();
--- morse.js
+++ morse.js
@@ -1,3 +1,8 @@
+// Écrire une fonction qui prend en entrée du code Morse et
+// le convertit en caractères lisibles par l’homme.
+// Le morse est composé de succession de points et de tirets.
+// Chaque lettre est séparée par un espace, chaque mot par 2 espaces
+
 'use strict';
 
 const alphabet = {
@@ -44,6 +49,44 @@ const alphabet = {
     ".": "E"
 };
 
+// let str = ".-.. .- .-.. .--. .... .- -... . -  -- --- .-. ... .  --- ..-  -.-. --- -.. .  -- --- .-. ... . --..--  . ... -  ..- -.  -.-. --- -.. .  .--. . .-. -- . - - .- -. -  -.. .  - .-. .- -. ... -- . - - .-. .  ..- -.  - . -..- - .   .-.. .- .. -.. .  -.. .  ... .-. .. . ...  -.. .. -- .--. ..- .-.. ... .. --- -. ...  -.-. --- ..- .-. - . ...  . -  .-.. --- -. --. ..- . ... --..--  --.- ..- . .-.. .-.. . ...  ... --- .. . -. -  .--. .-. --- -.. ..- .. - . ...  .--. .- .-.  -.. . ...  ... .. --. -. . ... --..--  ..- -. .  .-.. ..- -- .. .-. . --..--  ..- -.  ... --- -.  --- ..-  ..- -.  --. . ... - . .-.-.-";
+
 module.exports = function (str) {
-    // your code here
+	// your code here
+
+	str = ".-.. .- .-.. .--. .... .- -... . -  -- --- .-. ... .  --- ..-  -.-. --- -.. .  -- --- .-. ... . --..--  . ... -  ..- -.  -.-. --- -.. .  .--. . .-. -- . - - .- -. -  -.. .  - .-. .- -. ... -- . - - .-. .  ..- -.  - . -..- - .   .-.. .- .. -.. .  -.. .  ... .-. .. . ...  -.. .. -- .--. ..- .-.. ... .. --- -. ...  -.-. --- ..- .-. - . ...  . -  .-.. --- -. --. ..- . ... --..--  --.- ..- . .-.. .-.. . ...  ... --- .. . -. -  .--. .-. --- -.. ..- .. - . ...  .--. .- .-.  -.. . ...  ... .. --. -. . ... --..--  ..- -. .  .-.. ..- -- .. .-. . --..--  ..- -.  ... --- -.  --- ..-  ..- -.  --. . ... - . .-.-.-";
+    // let len = str.length;
+
+	// Let's caclulate the number of words, which is equal to thr number of blank
+	// spaces + 1. Reminder: 1 blank space = "  "
+    let nWords = str.match(/  /g).length + 1;
+
+    let wEnd = 0;
+    let subStr = str.substring(wEnd);
+    let word = "";
+    let lEnd = 0;
+    let subWord = "";
+    let letter = "";
+
+    let i = 0;
+    while (i < nWords) {
+    	wEnd = subStr.indexOf("  ");
+    	word = subStr.substring(0, wEnd);
+    	subStr = subStr.substring(wEnd);
+    	
+    	let j = 0;
+    	while (j < wEnd) {
+    		lEnd = word.indexOf(" ");
+    		letter = word.substring(0, lEnd);
+    		console.log(letter);
+    		word = word.substring("  ");
+    		j++;
+    	}
+
+    	i++;
+    	console.log("word");
+    }
+
+    // console.log(nwords, typeof(nwords), wIni, wEnd);
+
 };
